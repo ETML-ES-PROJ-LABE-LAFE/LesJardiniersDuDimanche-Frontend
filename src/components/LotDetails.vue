751e846a47542f38cada29b26fb7607071a81dd0
Samuel Mottier <sammot@hotmail.ch>
2024-05-06 14:24:03 +0200

    Update : Amélioration mise en page des lots

--- src/components/LotDetails.vue
+++ src/components/LotDetails.vue
@@ -3,8 +3,27 @@
 <template>
   <div class="lot-details">
     <h2>{{ lot.nom }}</h2>
-    <p>{{ lot.description }}</p>
-    <!-- Ajoutez ici d'autres détails du lot -->
+    <div class="lot-info">
+      <div class="lot-info-item">
+        <strong>Catégorie:</strong> {{ lot.categorie }}
+      </div>
+      <div class="lot-info-item">
+        <strong>ID:</strong> {{ lot.id }}
+      </div>
+      <div class="lot-info-item">
+        <strong>Description:</strong> {{ lot.description }}
+      </div>
+      <div class="lot-info-item">
+        <strong>Prix de départ:</strong> {{ lot.prixDepart }} CHF
+      </div>
+      <div class="lot-info-item">
+        <strong>Date et heure de début:</strong> {{ lot.dateHeureDebut }}
+      </div>
+      <div class="lot-info-item">
+        <strong>Date et heure de fin:</strong> {{ lot.dateHeureFin }}
+      </div>
+      <!-- Ajoutez ici d'autres détails du lot -->
+    </div>
   </div>
 </template>
 
@@ -15,5 +34,19 @@ export default {
 </script>
 
 <style scoped>
-/* Style spécifique pour les détails du lot */
+.lot-details {
+  background-color: #e5e5e5;
+  border: 1px solid #ccc;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 4px 4px rgba(0,0,0,0.3);
+}
+
+.lot-info {
+  margin-top: 20px;
+}
+
+.lot-info-item {
+  margin-bottom: 10px;
+}
 </style>
