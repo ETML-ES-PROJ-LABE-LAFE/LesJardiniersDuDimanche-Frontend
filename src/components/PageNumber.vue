95ebf47a719a94a222d491524c5e64dca34acb17
julie <juliecobas@outlook.com>
2024-06-06 12:00:20 +0200

    feat : ajout composant PageNumber pour la pagination des lots

---
+++ src/components/PageNumber.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="pagination">
+    <button @click="prevPage" :disabled="currentPage === 1">Précédent</button>
+    <span>Page {{ currentPage }} sur {{ totalPages }}</span>
+    <button @click="nextPage" :disabled="currentPage === totalPages">Suivant</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageNumber',
+  props: {
+    currentPage: {
+      type: Number,
+      required: true
+    },
+    totalPages: {
+      type: Number,
+      required: true
+    }
+  },
+  methods: {
+    prevPage() {
+      if (this.currentPage > 1) {
+        this.$emit('change-page', this.currentPage - 1);
+      }
+    },
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.$emit('change-page', this.currentPage + 1);
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.pagination {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+}
+
+button {
+  margin: 0 10px;
+  padding: 5px 10px;
+  border: none;
+  background-color: #ff4d4d;
+  color: white;
+  cursor: pointer;
+  border-radius: 4px;
+  font-size: 14px;
+}
+
+button:disabled {
+  background-color: #cccccc;
+  cursor: not-allowed;
+}
+</style>
