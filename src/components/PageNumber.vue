a2b340304a2861e8fb6166dd56221698e283cbb2
julie <juliecobas@outlook.com>
2024-06-06 15:13:27 +0200

    fix : Ajustement CSS Bouton Pagination

--- src/components/PageNumber.vue
+++ src/components/PageNumber.vue
@@ -42,15 +42,21 @@ export default {
   margin-top: 20px;
 }
 
+
 button {
   margin: 0 10px;
-  padding: 5px 10px;
+  padding: 10px 20px; /* Taille du padding pour des boutons plus grands */
   border: none;
-  background-color: #ff4d4d;
+  background-color: #007BFF; /* Nouvelle couleur bleu */
   color: white;
   cursor: pointer;
   border-radius: 4px;
-  font-size: 14px;
+  font-size: 18px; /* Taille de la police pour des boutons plus grands */
+  transition: background-color 0.3s ease; /* Ajout de transition pour effet visuel */
+}
+
+button:hover:not(:disabled) {
+  background-color: #0056b3; /* Couleur pour l'effet hover */
 }
 
 button:disabled {
