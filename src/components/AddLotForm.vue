f3d7032ba5407d475d1e8bc47c508781d1c4713a
Samuel Mottier <sammot@hotmail.ch>
2024-06-14 17:11:11 +0200

    Mise a jour formulaire création pour avoir l'état En cours  par défaut pour les lots

--- src/components/AddLotForm.vue
+++ src/components/AddLotForm.vue
@@ -58,7 +58,8 @@ export default {
         startingPrice: null,
         endingDateHours: "",
         category: { id: null, name: "" },
-        subCategory: { id: null, name: "" }
+        subCategory: { id: null, name: "" },
+        state: null
       },
       errors: {
         name: false,
